64e949607816064125f70724f1cbdfd37429746b
Ajibade Samson <94762634+Samhajj@users.noreply.github.com>
2023-03-06 21:38:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".express-btn").addEventListener("click", function () {
+  document.querySelector(".express-btn").style.background = "#000";
+  document.querySelector(".express-btn2").style.background = "#808080";
+});
+document.querySelector(".express-btn2").addEventListener("click", function () {
+  document.querySelector(".express-btn2").style.background = "#000";
+  document.querySelector(".express-btn").style.background = "#808080";
+});
